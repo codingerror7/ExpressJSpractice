f87c1ba32e1b538690f3617fdc000992eab3c69b
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-05-09 01:26:10 +0530

    script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let port = 3000;
 app.use("/",(req,res)=>{
     res.send("THIS IS MY FIRST BACKEND SERVER!");
 })
+// app.get("/about",(req,res)=>{
+//     res.send("THIS IS ABOUT-US PAGE!");
+// })
+// app.get("/services",(req,res)=>{
+//     res.send("THIS IS SERVICES PAGE!");
+// })
 app.listen(port,()=>{
     console.log(`WELCOME, YOU ARE AT PORT ${port}`);
 })
